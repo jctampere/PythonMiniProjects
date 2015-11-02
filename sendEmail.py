77e5b80f8ab28429129d504257e197539c8b7dd2
Jie Tampere <jie.j.chen.c@gmail.com>
2015-11-02 15:37:28 +0200

    send email python script

---
+++ sendEmail.py
@@ -0,0 +1,17 @@
+import smtplib
+
+# Create stmp connection obj
+# SMTP Servers: Gmail smtp.gmail.com; outlook/hotmail: smtp-mail.outlook.com; 
+# Yahoo: smtp.mail.yahoo.com; port is usually 587
+conn = smtplib.SMTP('smtp.gmail.com', 587)
+# Connect to the email server
+conn.ehlo()
+# Start encrytion in order to login
+conn.starttls()
+# Login, gmail use application specific password
+conn.login('username@gmail.com', 'password')
+
+# Send email, return value of sendmail is dict with all failed emails
+conn.sendmail('fromaddress', 'toaddress', 'Subject: subject \n\nMain body for the email\n\n')
+# Disconnect the connection
+conn.quit()
